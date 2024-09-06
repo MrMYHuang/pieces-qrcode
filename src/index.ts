74aedbb44badef4f5f702622cbc9edb26ceb51f0
Meng-Yuan Huang <myh@live.com>
2024-09-06 09:55:47 +0800

    fix bugs

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-import { createPiece } from '@activepieces/pieces-framework'
+import { createPiece, PieceAuth } from '@activepieces/pieces-framework'
 import { outputQrcodeAction } from './lib/actions/output-qrcode-action'
 
 export const qrcode = createPiece({
+  auth: PieceAuth.None(),
   displayName: 'QR Code',
   logoUrl: 'https://github.com/MrMYHuang/pieces-qrcode/blob/main/qrcode.png?raw=true',
   actions: [
     outputQrcodeAction,
   ],
   triggers: [
-  ]
+  ],
+  authors: ['Meng-Yuan Huang'],
 })
